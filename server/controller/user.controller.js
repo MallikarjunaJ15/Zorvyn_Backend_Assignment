8f7dfe1448cf0285039b0f30f8d1eeaa05a503e0
MallikarjunaDiv <bhajarangi05@gmail.com>
2026-04-06 21:55:46 +0530

    fix: ensure start script for render

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -14,8 +14,8 @@ export const register = async (req, res) => {
     res
       .cookie("token", data.token, {
         httpOnly: true,
-        secure: false,
-        sameSite: "lax",
+        secure: true,
+        sameSite: "None",
       })
       .status(200)
       .json(data);
@@ -30,8 +30,8 @@ export const login = async (req, res) => {
     res
       .cookie("token", data.token, {
         httpOnly: true,
-        secure: false,
-        sameSite: "lax",
+        secure: true,
+        sameSite: "none",
       })
       .status(200)
       .json(data);
